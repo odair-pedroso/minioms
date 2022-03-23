79945747b93cad376d7f49fa9c10a37cb1a353f7
odair-pedroso <odairpedroso90@gmail.com>
2022-03-23 19:19:13 -0300

    ajuste da documentação api swagger

--- src/customer.model.ts
+++ src/customer.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 
@@ -8,25 +9,45 @@ export class Customer extends Model<Customer>{
     type: DataType.STRING(100),
     allowNull: false,
   })
+  @ApiProperty({
+    example: 'Odair Pedroso',
+    description: 'Nome do cliente',
+  })
   name: string;
   @Column({
     type: DataType.STRING(11),
     allowNull: false,
+  })
+  @ApiProperty({
+    example: '99999999999',
+    description: 'Número de CPF do cliente',
   })
   cpf: string;
   @Column({
     type: DataType.STRING(14),
     allowNull: false,
   })
-  cnpj: string;
+  @ApiProperty({
+    example: '11111111111111',
+    description: 'Número do CNPJ do cliente',
+  })
+  cnpj?: string;
   @Column({
     type: DataType.STRING(100),
     allowNull: false,
+  })
+  @ApiProperty({
+    example: 'example@gmail.com',
+    description: 'E-mail do cliente',
   })
   email: string;
   @Column({
     type: DataType.STRING(15),
     allowNull: false,
+  })
+  @ApiProperty({
+    example: '1199999-9999',
+    description: 'Número do telefone do cliente',
   })
   phone: string;
 }
