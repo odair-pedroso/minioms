32ef93856dfa507e46852a56c6c9b0e4701f83c0
odair-pedroso <odairpedroso90@gmail.com>
2022-03-23 22:44:09 -0300

    add status de retorno aos endpoints

--- src/customers.controller.ts
+++ src/customers.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Customer } from "./customer.model";
 import { CustomersService } from "./customers.service";
 
@@ -11,28 +11,26 @@ export class CustomersController {
   }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'The customer list has been displayed successfully.' })
   async findAll(): Promise<Customer[]> {
     return this.customersService.findAll();
   }
-  @Get(':id')  
+  @Get(':id')
+  @ApiResponse({ status: 200, description: 'The customer has been displayed successfully.' })
   async findOne(@Param() params): Promise<Customer> {
     return this.customersService.findOne(params.id);
   }
 
   @Post()
-  @ApiResponse({status: 201, description: 'The record has been successfully created.'})
+  @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
   async create(@Body() customer: Customer) {
     this.customersService.create(customer);
   }
 
-  
+
   @Delete(':id')
+  @ApiResponse({ status: 200, description: 'The client was successfully deleted' })
   async delete(@Param() params) {
     this.customersService.delete(params.id);
   }
-
-
-
-
-
 }
